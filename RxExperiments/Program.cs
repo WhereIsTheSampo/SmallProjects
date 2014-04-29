974e8c10876b7c65c93aa7aa0c03af35808840c0
Bob Reid <rjreid3000@gmail.com>
2014-04-28 22:12:22 -0600

    experimenting

--- RxExperiments/Program.cs
+++ RxExperiments/Program.cs
@@ -4,12 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Reactive;
+using System.Reactive.Linq;
+
 namespace RxExperiments
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(String[] args)
         {
+            //var observable = Observable.Return("return value");
+            //var observable = Observable.Empty<String>();
+            //var observable = Observable.Never<String>();
+            //var observable = Observable.Throw<String>(new Exception("test exception"));
+
+
+            observable.Subscribe(s => HandleNext(s), ex => HandleError(ex), HandleCompleted);
+
+            Console.ReadLine();
         }
+
+        private static void HandleNext(String item)
+        {
+            Console.WriteLine("Next: " + item.ToString());
+        }
+
+        private static void HandleError(Exception exception)
+        {
+            Console.WriteLine("Error: " + exception.ToString());
+        }
+
+        private static void HandleCompleted()
+        {
+            Console.WriteLine("Completed");
+        }
+
     }
 }
